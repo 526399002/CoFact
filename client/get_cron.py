5bb7539bbb382276f888f7f43260530ed249eacc
root <root@node1.pixiu.com>
2017-02-19 22:22:20 +0800

    edit: 完善client端的配置
    添加传输数据项

--- client/get_cron.py
+++ client/get_cron.py
@@ -1,8 +1,24 @@
 import re
-import struct
 import glob
 import json
-from socket import socket, AF_INET, SOCK_STREAM
+import struct
+import logging
+from datetime import datetime
+from threading import Thread, Event
+from socket import socket, AF_INET, SOCK_STREAM, gethostname
+
+__version__ = "0.0.1"
+
+
+def get_ip():
+    try:
+        sock = socket.socket(socket.AF_INET, SOCK_STREAM)
+        sock.connect(('8.8.8.8', 80))
+        address, port = sock.getsockname()
+        sock.close()
+        return address
+    except socket.error:
+        return "127.0.0.1"
 
 
 def encode_data(data):
@@ -10,12 +26,15 @@ def encode_data(data):
     length = len(data)
     return struct.pack("<l{}s".format(length). length, data)
 
+
 class GetCron:
+    event = Event()
+
     def __init__(self, master):
         self.master = master
         self.user_cron = []
         self.crontab = []
-        self.data = {}
+        self.all_cron = {}
         self.files = glob.glob("/var/spool/cron/*")
         self.so = None
 
@@ -23,20 +42,20 @@ class GetCron:
         self.so = socket(AF_INET, SOCK_STREAM)
         self.so.connect(self.master)
 
-    def read_cron(self): 
+    def col_cron(self): 
         for files in self.files:
             with open(i) as f:
                 for i in f:
                     self.user_cron.append(i.rstrip("\n")) 
-        self.data["user_cron"] = self.user_cron
+        self.all_cron["user_cron"] = self.user_cron
 
-    def read_crontab(self):
+    def col_crontab(self):
         with open("/etc/crontab") as f:
             for line in f:
                 if not ("=" in line or line.startswith(("#", " #")) or line == "\n"):
                     self.crontab.append(line)
-        self.data["crontab"] = self.crontab
-   
+        self.all_cron["crontab"] = self.crontab
+
     def tail_n(self, files, interval=10, num=10):
         pos = 0
         while True:
@@ -49,7 +68,44 @@ class GetCron:
                 if len(rest) > num:
                     return rest[::-1][:num]
 
+    def hearbeat(self):
+        self.col_crontab()
+        self.col_cron()
+        data = {
+            'host': gethostname,
+            "ip": get_ip(),
+            "version": __version__,
+            "timestamp": datetime.now().timestamp(),
+            "cron_fact": self.data,
+        }
+        try:
+            self.so.send(encode(data))
+        except Exception as e:
+            logging.error("send heartbeat error: {}".format(e))
+            self.connect()
+
+    def send(self):
+        self.connect()
+        while not event.is_set():
+            self.read_cron()
+            self.read_crontab()
+            data = encode_data(self.data)
+            self.socket.send(data)
+            event.wait(5)
+  
+    def start(self):
+        t = Thread(target=self.send)
+        try:
+            t.start()
+        except:
+            self.event.set()
+
+    def shutdown(self):
+        self.event.set()
+
 if __name__ == '__main__':
-    get = GetCron()
-    get.read_crontab()
-    print(get.crontab)
+    cron = GetCron()
+    try:
+        cron.start()
+    except KeyboardInterrupt:
+        cron.shutdown()
